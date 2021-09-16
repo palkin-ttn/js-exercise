190843489ff34b61eb0fd6634f7c06f46ab2a129
palkin-ttn <90325761+palkin-ttn@users.noreply.github.com>
2021-09-16 18:34:36 +0530

    Add files via upload

---
+++ employee-info/employee_info.js
@@ -0,0 +1,19 @@
+emp = new Array()
+emp[0] = { name: "Palkin", age: '21', salary: 7500, DOB: "08-10-1999" }
+emp[1] = { name: "Milan", age: '30', salary: 9000, DOB: "12-03-1991" }
+emp[2] = { name: "Sakshi", age: '26', salary: 500, DOB: "27-05-1995" }
+emp[3] = { name: "Prachi", age: '18', salary: 400, DOB: "30-07-2003" }
+emp[4] = { name: "Kashish", age: '25', salary: 4500, DOB: "01-09-1996" }
+emp[5] = { name: "Megha", age: '19', salary: 400, DOB: "11-07-2002" }
+emp[6] = { name: "Ankit", age: '16', salary: 400, DOB: "06-08-2005" }
+emp[7] = { name: "Aman", age: '24', salary: 400, DOB: "19-01-1997" }
+emp[8] = { name: "Ram", age: '27', salary: 400, DOB: "04-04-1994" }
+emp[9] = { name: "Sonam", age: '23', salary: 8000, DOB: "17-06-1998" }
+emp[10] = { name: "Riya", age: '22', salary: 400, DOB: "10-09-1999" }
+ 
+for (var i = 0; i < emp.length; i++)
+{
+ 
+    console.log(emp[i].name, emp[i].age, emp[i].salary, emp[i].DOB)
+}
+ 
