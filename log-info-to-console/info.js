51e87c92ec401d6b6cff0be5efc51ee7ac0e9714
palkin-ttn <90325761+palkin-ttn@users.noreply.github.com>
2021-09-19 22:57:25 +0530

    Update info.js

--- log-info-to-console/info.js
+++ log-info-to-console/info.js
@@ -1,6 +1,7 @@
-function obj() {
-    console.log("original")
-    var d = a+b+c
-}
-copy = obj
-console.log(copy)
+let obj = {name: "Palkin", age: 21 };
+document.write("This is original Object\n");
+document.write("Name:" + " " + obj.name + "\n" + "Age:" + " " + obj.age);
+let copyObj = Object.assign({}, obj);
+document.write("\nThis is the copy of original Object\n");
+document.write("Name:" + " " + copyObj.name + "\n" + "Age:" + " " + copyObj.age)
+console.log("Test");
